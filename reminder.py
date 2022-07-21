52a4097353906e220e6ccc7d71a775e0720b08c9
M.E <52017620+acroscopic@users.noreply.github.com>
2022-07-21 15:31:06 -0500

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -1,19 +1,8 @@
-#This is a script that runs once to send a 1 off reminder to the email, it's relatively simple because I don't use it often.
-
-import os
-import time
 import sys
 import getpass
 import smtplib, ssl
-import schedule
-import threading
-import random
 import requests
 import numpy as np
-from datetime import date
-
-t = date.today()
-startdate = t.strftime("%m/%d/%y")
 
 port = 465
 context = ssl.create_default_context()
@@ -21,8 +10,7 @@ context = ssl.create_default_context()
 sender_email = input("Please enter the sending email")
 password = getpass.getpass("Input password and press enter:")
 receiver_email = input("Please enter the receiving email")
-emailtime = input("Please enter the time you want to be reminded in 24 format: ")
-message = input("Please enter your reminder message now: ")
+message = input("Please enter your message: ")
 
 def Reminder():
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
@@ -32,10 +20,4 @@ def Reminder():
             server.sendmail(sender_email, receiver_email, i)
             sys.exit("Reminder has been sent!")
 
-#Setting the time for each email array
-schedule.every().day.at(emailtime).do(Reminder)
-
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+Reminder()
